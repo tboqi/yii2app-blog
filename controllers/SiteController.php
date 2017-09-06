19e71a975d184a8d657eef3b220d36d73b61f445
tangbq <tangbq@tiens.com>
2017-09-06 14:32:49 +0800

    update from dell msysgit

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,12 +6,10 @@ use blog\models\PasswordResetRequestForm;
 use blog\models\ResetPasswordForm;
 use blog\models\SignupForm;
 use common\models\LoginForm;
+use qiyu\Controller;
 use Yii;
 use yii\base\InvalidParamException;
-use yii\filters\AccessControl;
-use yii\filters\VerbFilter;
 use yii\web\BadRequestHttpException;
-use yii\web\Controller;
 
 /**
  * Site controller
@@ -19,60 +17,13 @@ use yii\web\Controller;
 class SiteController extends Controller
 {
     /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
-    /**
      * Displays homepage.
      *
      * @return mixed
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->render('index.tpl');
     }
 
     /**
